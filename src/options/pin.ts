645df0c8d08d8b30bd325450dbca7e4ad4e9d342
Derrick Hammer <derrick@derrickhammer.com>
2024-03-22 18:11:17 -0400

    feat: add pin and unpin api

---
+++ src/options/pin.ts
@@ -0,0 +1,8 @@
+import {CustomClientOptions} from "#utils/options.js";
+import {ResponseType} from "axios";
+
+export type CustomPinOptions = CustomClientOptions & {
+
+};
+
+export const DEFAULT_PIN_OPTIONS = {};
